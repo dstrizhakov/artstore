0b9111e9c594349460a14995d088af5f1be9323f
dmirty.s <d.strizhakov@gmail.com>
2023-08-08 19:14:09 +0300

    feat: add redux, react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
+import { setupStore } from 'store/store.ts';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const store = setupStore();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
