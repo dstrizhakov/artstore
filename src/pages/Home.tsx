e197525ab3e7eec7f98d2fe20cb162a49e5f0a39
dmirty.s <d.strizhakov@gmail.com>
2023-08-09 21:02:55 +0300

    feat: add sdk, add login inputs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import Button from '@mui/material/Button';
 
 const Home: FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,25 @@
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 import { FC } from 'react';
+import styles from './Login.module.scss';
+import { Link } from 'react-router-dom';
 
 const Login: FC = () => {
+  const handleLogin = () => {};
+
   return (
     <div>
       <h1>Login Page</h1>
+      <div className={styles.wrapper}>
+        <TextField id="outlined-basic" label="email" variant="outlined" />
+        <TextField id="outlined-basic" label="password" variant="outlined" type="password" />
+        <p>
+          Dont have account <Link to="/register">Register</Link>
+        </p>
+        <Button onClick={handleLogin} variant="contained">
+          Login
+        </Button>
+      </div>
     </div>
   );
 };
