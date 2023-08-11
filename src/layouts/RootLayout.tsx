59da04452ae70c7a4bc30cfcfcc3698f43bf3c07
Stanislav <stanislav.shendryk@gmail.com>
2023-08-11 21:59:56 +0300

    feat: add parameters for routing

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from 'react';
+import { Outlet } from 'react-router';
+import Header from '../components/Header/Header';
+
+const RootLayout: FC = (): ReactNode => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+
+    </div>
+  );
+};
+
+export default RootLayout;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -4,6 +4,7 @@ const Shop: FC = () => {
   return (
     <div>
       <h1>Shop Page</h1>
+      <a href="/shop/id">Product Details</a>
     </div>
   );
 };
