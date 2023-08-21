f961f6de1639042ba2fc03564931c17507478206
Stanislav <stanislav.shendryk@gmail.com>
2023-08-21 20:38:43 +0300

    feat: add countries

---
+++ src/constants/countries.tsx
@@ -0,0 +1,50 @@
+export const countries = [
+  {
+    name: 'Canada',
+    value: 'CA',
+  },
+  {
+    name: 'Denmark',
+    value: 'DK',
+  },
+  {
+    name: 'Spain',
+    value: 'ES',
+  },
+  {
+    name: 'France',
+    value: 'FR',
+  },
+  {
+    name: 'United Kingdom',
+    value: 'UK',
+  },
+  {
+    name: 'Georgia',
+    value: 'GE',
+  },
+  {
+    name: 'Italy',
+    value: 'IT',
+  },
+  {
+    name: 'Japan',
+    value: 'JP',
+  },
+  {
+    name: 'Russia',
+    value: 'RU',
+  },
+  {
+    name: 'Turkey',
+    value: 'TR',
+  },
+  {
+    name: 'Ukraine',
+    value: 'UA',
+  },
+  {
+    name: 'Belarus',
+    value: 'BY',
+  },
+];
