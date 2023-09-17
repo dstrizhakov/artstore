7156f9188c057e19fd4e11f4d020824c414f0aff
Stanislav <stanislav.shendryk@gmail.com>
2023-09-17 17:05:34 +0300

    feat: add info for rsschool

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import AboutList from '../components/AboutList/AboutList';
+import RsschoolDescription from '../components/RsschoolDescription/RsschoolDescription';
 
 const About: FC = () => {
   return (
     <div>
       <h2>About Us</h2>
       <AboutList />
+      <RsschoolDescription />
     </div>
   );
 };
